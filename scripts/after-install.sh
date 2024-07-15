ab1cf80b1b1a5aecc8936df4fc223765afd07382
Himanshu-0711 <himanshuwagh23@gmail.com>
2024-07-15 14:47:24 +0530

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Example: Change ownership and permissions of files in /var/www/html
-sudo chown -R nginx:nginx /var/www/html
-sudo chmod -R 755 /var/www/html
+docker pull 909325007152.dkr.ecr.us-east-1.amazonaws.com/dev:latest
+
 
 # Example: Start NGINX service after deployment
-sudo service nginx start
+docker run -d -p 80:80 dev:latest
 
 # Example: Additional tasks after installation
 # ...
